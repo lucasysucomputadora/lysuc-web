b9a5f7c08470dd7758d1639c327df91d1c7443cb
lucasysucomputadora <carcasaradiante@gmail.com>
2020-09-04 15:20:37 -0300

    Updates

---
+++ precache-manifest.208ed6af4beec3263b130f0de11161a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84b08bd4c2be57080d6dc196ca1e350",
+    "url": "/flashando-algo/index.html"
+  },
+  {
+    "revision": "c5f6ed2c3e0ecd4a4cf4",
+    "url": "/flashando-algo/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "6a147d6ca6cc60d9c1f8",
+    "url": "/flashando-algo/static/css/main.28bed3b7.chunk.css"
+  },
+  {
+    "revision": "c5f6ed2c3e0ecd4a4cf4",
+    "url": "/flashando-algo/static/js/2.5afdaf37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flashando-algo/static/js/2.5afdaf37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a147d6ca6cc60d9c1f8",
+    "url": "/flashando-algo/static/js/main.5875015c.chunk.js"
+  },
+  {
+    "revision": "3fc637e8ff9a3f77a629",
+    "url": "/flashando-algo/static/js/runtime-main.d824714d.js"
+  }
+]);
