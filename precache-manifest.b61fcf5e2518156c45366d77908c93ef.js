aecf02480a4733c35befa50ac6349f31c8287825
lucasysucomputadora <carcasaradiante@gmail.com>
2020-09-04 16:24:29 -0300

    Updates

--- precache-manifest.b61fcf5e2518156c45366d77908c93ef.js
+++ precache-manifest.b61fcf5e2518156c45366d77908c93ef.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e2fb9a65cad0250abe218d640a4d6702",
+    "revision": "bcf80652270654e77e2109c3cf7140a1",
     "url": "/lysuc-web/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lysuc-web/static/css/2.9131a3d6.chunk.css"
   },
   {
-    "revision": "4cce4373d93b4b86da7d",
+    "revision": "b0d684df7d3cacac1510",
     "url": "/lysuc-web/static/css/main.28bed3b7.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lysuc-web/static/js/2.5afdaf37.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4cce4373d93b4b86da7d",
-    "url": "/lysuc-web/static/js/main.df597a07.chunk.js"
+    "revision": "b0d684df7d3cacac1510",
+    "url": "/lysuc-web/static/js/main.6999f102.chunk.js"
   },
   {
     "revision": "86e69dec686bd82fe5e5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lysuc-web/precache-manifest.5a80db40d813b55a38c22c4d8b913524.js"
+  "/lysuc-web/precache-manifest.b61fcf5e2518156c45366d77908c93ef.js"
 );
 
 self.addEventListener('message', (event) => {
